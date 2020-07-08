d551712acf0b765365f18c2ee405e4238be38567
Anup <anupkhalam@gmail.com>
2020-07-09 00:04:50 +0530

    README file added

---
+++ README.md
@@ -0,0 +1,35 @@
+# Overview
+This repository provides a Python script to track the internet speed at user defined intervals. This utility was developed under a scenario in which my ISP was bothering me regarding my complaint on low bandwidth.
+
+## System set-up (tested only on Ubuntu 18.04 LTS)
+1. Create virtual environment and activate it.
+
+2. Clone this repository
+~~~
+git clone https://github.com/anupkhalam/internet_speed_testing.git
+~~~
+
+3. Install required packages
+~~~
+pip3 install -r requirements.txt
+~~~
+
+4. Install nohup if you do not want to keep open a terminal
+~~~
+sudo apt-get install nohup
+~~~
+
+5. Run the following
+~~~
+nohup python3 track_speed.py --speed <s> --interval <i> --records <r> &
+~~~
+    **\<s>**: Expected speed (say 50)
+
+    **\<i>**: Interval in which speed needs to be tracked in minutes (say 60)
+
+    **\<r>**: Number of records to be reported (say 24)
+
+    Then, run the following
+~~~
+nohup python3 track_speed.py --speed 50 --interval 60 --records 24 &
+~~~
--- track_speed.py
+++ track_speed.py
@@ -8,13 +8,13 @@ Created on Wed Jul  8 18:08:38 2020.
 
 import speedtest
 import datetime
+import argparse
 import pandas as pd
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-def get_internet_speed():
+def get_internet_speed(args_):
     """Get internet speed."""
-    print("*********************")
     dt = datetime.datetime.now()
     details = {}
     details['Time'] = [dt.strftime("%X")]
@@ -24,29 +24,45 @@ def get_internet_speed():
     s.get_best_server()
     s.download(threads=None)
     s.upload(threads=None)
-    s.results.share()
     results_dict = s.results.dict()
     details['Download speed (Mbps)'] = [round(results_dict['download'] /
                                               (1024*1024), 2)]
     details['Upload speed (Mbps)'] = [round(results_dict['download'] /
                                             (1024*1024), 2)]
-    dataset = pd.read_csv('data/consolidated.csv')
-    df1 = pd.DataFrame(details, columns=['Date',
-                                         'Day',
-                                         'Time',
-                                         'Download speed (Mbps)',
-                                         'Upload speed (Mbps)'])
+    df_columns = ['Date',
+                  'Day',
+                  'Time',
+                  'Download speed (Mbps)',
+                  'Upload speed (Mbps)']
+    try:
+        dataset = pd.read_csv('data/consolidated.csv')
+    except FileNotFoundError:
+        dataset = pd.DataFrame(columns=df_columns)
+    df1 = pd.DataFrame(details, columns=df_columns)
     dataset = dataset.append(df1, ignore_index=True)
     dataset.to_csv('data/consolidated.csv', index=None)
 
-    if len(dataset) > 24:
-        df2 = dataset.tail(24)
+    if len(dataset) >= args['records']:
+        df2 = dataset.tail(args['records'])
         avg_download_speed = dataset['Download speed (Mbps)'].mean()
         avg_upload_speed = dataset['Upload speed (Mbps)'].mean()
-        if avg_download_speed < 50. and avg_upload_speed < 50.:
+        if avg_download_speed < float(args['speed']) and \
+           avg_upload_speed < float(args['speed']):
             df2.to_excel('data/speed_report.xlsx', index=None)
 
 
-scheduler = BlockingScheduler()
-scheduler.add_job(get_internet_speed, 'interval', hours=1)
-scheduler.start()
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-s", "--speed", required=True,
+                    help="Expected internet speed")
+    ap.add_argument("-i", "--interval", required=True,
+                    help="Interval in minutes")
+    ap.add_argument("-i", "--records", required=True,
+                    help="Number of records to capture in the report")
+    args = vars(ap.parse_args())
+    scheduler = BlockingScheduler()
+    scheduler.add_job(func=get_internet_speed,
+                      trigger='interval',
+                      args=[args],
+                      minutes=int(args['interval']))
+    scheduler.start()
